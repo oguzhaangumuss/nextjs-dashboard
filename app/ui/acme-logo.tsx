1cfaaf201098cde280f842aaeed762e0d2a699e7
Oğuzhan Gümüş <oguzhaangumuss@hotmail.com>
2024-01-31 00:28:43 +0300

    Connecting to Database, Fetching Data, Static and Dynamic Rendering, Streaming

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,9 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
+      <p className="text-[37px]">Oğuzhan</p>
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      
     </div>
   );
 }
